475934e16c2741361b171d740dbf071f42974051
Christopher D. Long <galizur@gmail.com>
2014-03-13 01:49:22 -0400

    Parse out team ID.

--- nfl/nfl_team_standings.rb
+++ nfl/nfl_team_standings.rb
@@ -9,20 +9,16 @@ base_sleep = 0
 sleep_increment = 3
 retries = 4
 
-#http://www.nfl.com/teams/newengland%20patriots/schedule?team=NE&season=2012&seasonType=REG
-
 #http://www.nfl.com/liveupdate/game-center/2013090500/2013090500_gtd.json
 #http://www.nfl.com/scores/2013/REG1
 
 base_url = "http://www.nfl.com"
 
-nfl_team_standings = CSV.open("csv/nfl_team_standings.csv","r",{:col_sep => "\t", :headers => TRUE})
-
-nfl_team_schedules = CSV.open("csv/nfl_team_standings.csv","w",{:col_sep => "\t"})
+nfl_team_standings = CSV.open("csv/nfl_team_standings.csv","w",{:col_sep => "\t"})
 
 # Header for team standings file
 
-nfl_team_standings << ["season", "team_name", "status", "team_url", "wins", "losses", "ties", "win_percentage", "points_for", "points_against", "net_points", "touchdowns", "home_record", "road_record", "division_record", "division_win_percentage", "conference_record", "conference_win_percentage", "nonconference_record","winning_streak","last_5_record"]
+nfl_team_standings << ["season", "team_id", "team_name", "status", "team_url", "wins", "losses", "ties", "win_percentage", "points_for", "points_against", "net_points", "touchdowns", "home_record", "road_record", "division_record", "division_win_percentage", "conference_record", "conference_win_percentage", "nonconference_record","winning_streak","last_5_record"]
 
 #AFC East Team	W	L	T	 Pct	PF	PA	Net Pts	TD	Home	Road	Div	Pct	Conf	Pct	Non-Conf	Streak	Last 5
 
@@ -73,18 +69,20 @@ team_standings_xpath = '//*[@class="data-table1"]/tbody/tr'
           team_name = parts[1].strip rescue nil
           link = field.xpath("a").first
           team_url = (base_url+link.attributes["href"].text) rescue nil
+          team_id = team_url.split("=")[1] rescue nil
         else
           status = nil
           team_name = parts[0].strip rescue nil
           link = field.xpath("a").first
           team_url = (base_url+link.attributes["href"].text) rescue nil
+          team_id = team_url.split("=")[1] rescue nil
         end
-        row += [team_name,status,team_url]
+        row += [team_id,team_name,status,team_url]
       else
         row << field.text.strip rescue nil
       end
     end
-    if (row.size>4) and not(row[4]=="W")
+    if (row.size>5) and not(row[5]=="W")
       nfl_team_standings << row
     end
 
